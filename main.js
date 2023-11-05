28fcc2c461cf3661ca34c75b842d4abede502323
DiademDev <118876421+DiademDev@users.noreply.github.com>
2023-11-06 09:25:04 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -20,42 +20,24 @@ const images = [
     'images/image_13.png',
     'images/image_14.png',
     'images/image_15.png',
-    'images/image_16.png',
-    'images/image_17.png',
-    'images/image_18.png',
-    'images/image_19.png',
-    'images/image_20.png',
-    'images/image_21.png',
-    'images/image_22.png',
-    'images/image_23.png',
-    'images/image_24.png',
 ];
 
 const texts = [
     'Sky Sign',
-    'Main ID Pylon',
+    'Main Identification Signage',
     'Interpretive',
     'Vertical Garden',
     'Swales',
     'Permeable Paving',
     'Tree Guards',
     'Living Pergola',
-    'Brand Text 9',
-    'Brand Text 10',
-    'Brand Text 11',
-    'Brand Text 12',
-    'Brand Text 13',
+    'Facade',
+    'Branded Portico',
+    'Interpretive',
+    'Outdoor Dining',
+    'Bike Racks',
     'Brand Text 14',
-    'Brand Text 15',
-    'Brand Text 16',
-    'Brand Text 17',
-    'Brand Text 18',
-    'Brand Text 19',
-    'Brand Text 20',
-    'Brand Text 21',
-    'Brand Text 22',
-    'Brand Text 23',
-    'Brand Text 24'
+    'Brand Text 15'
 ];
 
 // Show the first 5 coordsButtons when the first picto is clicked
@@ -82,23 +64,23 @@ pictos[2].addEventListener('click', () => {
 // Show coordsButtons 12-16 when picto4 is clicked
 pictos[3].addEventListener('click', () => {
     coordsButtons.forEach((button, index) => {
-        button.style.display = index >= 13 && index < 17 ? 'block' : 'none';
+        button.style.display = index >= 8 && index < 11 ? 'block' : 'none';
     });
 });
 
 // Show coordsButtons 17-21 when picto4 is clicked
 pictos[4].addEventListener('click', () => {
     coordsButtons.forEach((button, index) => {
-        button.style.display = index >= 17 && index < 21 ? 'block' : 'none';
+        button.style.display = index >= 11 && index < 13 ? 'block' : 'none';
     });
 });
 
-// Show coordsButtons 21-25 when picto4 is clicked
-pictos[5].addEventListener('click', () => {
-    coordsButtons.forEach((button, index) => {
-        button.style.display = index >= 21 && index < 25 ? 'block' : 'none';
-    });
-});
+// // Show coordsButtons 21-25 when picto4 is clicked
+// pictos[5].addEventListener('click', () => {
+//     coordsButtons.forEach((button, index) => {
+//         button.style.display = index >= 21 && index < 25 ? 'block' : 'none';
+//     });
+// });
 
 pictos.forEach((picto, index) => {
     picto.addEventListener('mouseenter', () => {
